morogoku/MTweaks-KernelAdiutorMOD

spectrum tile request less root access

app/src/main/java/com/moro/mtweaks/services/boot/ApplyOnBootService.java
@@ -43,6 +43,7 @@ import com.moro.mtweaks.utils.kernel.cpuvoltage.VoltageCl0;
 import com.moro.mtweaks.utils.kernel.cpuvoltage.VoltageCl1;
 import com.moro.mtweaks.utils.kernel.gpu.GPUFreqExynos;
 import com.moro.mtweaks.utils.kernel.boefflawakelock.BoefflaWakelock;
+import com.moro.mtweaks.utils.kernel.spectrum.Spectrum;
 import com.moro.mtweaks.utils.root.RootUtils;
 
 /**
@@ -88,6 +89,10 @@ public class ApplyOnBootService extends Service {
         if(BoefflaWakelock.supported()) {
             BoefflaWakelock.CopyWakelockBlockerDefault();
         }
+
+        // update spectrum support and profile
+        AppSettings.saveBoolean("spectrum_supported", Spectrum.suSupported(), this);
+        AppSettings.saveInt("spectrum_profile", Spectrum.getSuProfile(), this);
 
         // Check if kernel is changed
         String kernel_old = AppSettings.getString("kernel_version_old", "", this);

app/src/main/java/com/moro/mtweaks/utils/kernel/spectrum/Spectrum.java
@@ -1,7 +1,10 @@
 package com.moro.mtweaks.utils.kernel.spectrum;
 
+import android.content.Context;
 import android.os.AsyncTask;
 
+import com.moro.mtweaks.utils.AppSettings;
+import com.moro.mtweaks.utils.Utils;
 import com.moro.mtweaks.utils.root.RootUtils;
 
 /**
@@ -10,32 +13,39 @@ import com.moro.mtweaks.utils.root.RootUtils;
 
 public class Spectrum {
 
-    public static String getProfile(){
-        return RootUtils.runCommand("getprop persist.spectrum.profile");
+    public static int getSuProfile(){
+        return Utils.strToInt(RootUtils.runCommand("getprop persist.spectrum.profile"));
+    }
+
+    static int getProfile(Context context){
+        return AppSettings.getInt("spectrum_profile", 0 , context);
     }
 
     // Method that interprets a profile and sets it
-    public static void setProfile(int profile) {
+    public static void setProfile(int profile, Context context) {
         int numProfiles = 3;
-        if (profile > numProfiles || profile < 0) {
-            setProp(0);
-        } else {
-            setProp(profile);
-        }
+        if (profile > numProfiles || profile < 0) profile = 0;
+
+        setProp(profile, context);
     }
 
     // Method that sets system property
-    private static void setProp(final int profile) {
+    private static void setProp(final int profile, Context context) {
         new AsyncTask<Object, Object, Void>() {
             @Override
             protected Void doInBackground(Object... params) {
+                AppSettings.saveInt("spectrum_profile", profile, context);
                 RootUtils.runCommand("setprop persist.spectrum.profile " + profile);
                 return null;
             }
         }.execute();
     }
 
-    public static boolean supported() {
-        return RootUtils.getProp("spectrum.support").equals("1");
+    public static boolean suSupported(){
+        return RootUtils.runCommand("getprop spectrum.support").equals("1");
+    }
+
+    public static boolean supported(Context context){
+        return AppSettings.getBoolean("spectrum_supported", false, context);
     }
 }

opensourcefreak/hKtweaks

app/src/main/java/com/hades/hKtweaks/services/boot/ApplyOnBootService.java
@@ -43,6 +43,7 @@ import com.hades.hKtweaks.utils.kernel.cpuvoltage.VoltageCl0;
 import com.hades.hKtweaks.utils.kernel.cpuvoltage.VoltageCl1;
 import com.hades.hKtweaks.utils.kernel.gpu.GPUFreqExynos;
 import com.hades.hKtweaks.utils.kernel.boefflawakelock.BoefflaWakelock;
+import com.hades.hKtweaks.utils.kernel.spectrum.Spectrum;
 import com.hades.hKtweaks.utils.root.RootUtils;
 
 /**
@@ -88,6 +89,10 @@ public class ApplyOnBootService extends Service {
         if(BoefflaWakelock.supported()) {
             BoefflaWakelock.CopyWakelockBlockerDefault();
         }
+
+        // update spectrum support and profile
+        AppSettings.saveBoolean("spectrum_supported", Spectrum.suSupported(), this);
+        AppSettings.saveInt("spectrum_profile", Spectrum.getSuProfile(), this);
 
         // Check if kernel is changed
         String kernel_old = AppSettings.getString("kernel_version_old", "", this);

app/src/main/java/com/hades/hKtweaks/utils/kernel/spectrum/Spectrum.java
@@ -1,7 +1,10 @@
 package com.hades.hKtweaks.utils.kernel.spectrum;
 
+import android.content.Context;
 import android.os.AsyncTask;
 
+import com.hades.hKtweaks.utils.AppSettings;
+import com.hades.hKtweaks.utils.Utils;
 import com.hades.hKtweaks.utils.root.RootUtils;
 
 /**
@@ -10,32 +13,39 @@ import com.hades.hKtweaks.utils.root.RootUtils;
 
 public class Spectrum {
 
-    public static String getProfile(){
-        return RootUtils.runCommand("getprop persist.spectrum.profile");
+    public static int getSuProfile(){
+        return Utils.strToInt(RootUtils.runCommand("getprop persist.spectrum.profile"));
+    }
+
+    static int getProfile(Context context){
+        return AppSettings.getInt("spectrum_profile", 0 , context);
     }
 
     // Method that interprets a profile and sets it
-    public static void setProfile(int profile) {
+    public static void setProfile(int profile, Context context) {
         int numProfiles = 3;
-        if (profile > numProfiles || profile < 0) {
-            setProp(0);
-        } else {
-            setProp(profile);
-        }
+        if (profile > numProfiles || profile < 0) profile = 0;
+
+        setProp(profile, context);
     }
 
     // Method that sets system property
-    private static void setProp(final int profile) {
+    private static void setProp(final int profile, Context context) {
         new AsyncTask<Object, Object, Void>() {
             @Override
             protected Void doInBackground(Object... params) {
+                AppSettings.saveInt("spectrum_profile", profile, context);
                 RootUtils.runCommand("setprop persist.spectrum.profile " + profile);
                 return null;
             }
         }.execute();
     }
 
-    public static boolean supported() {
-        return RootUtils.getProp("spectrum.support").equals("1");
+    public static boolean suSupported(){
+        return RootUtils.runCommand("getprop spectrum.support").equals("1");
+    }
+
+    public static boolean supported(Context context){
+        return AppSettings.getBoolean("spectrum_supported", false, context);
     }
 }